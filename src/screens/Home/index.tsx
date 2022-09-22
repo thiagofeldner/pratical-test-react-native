0ce38d6007f3ec5fa97923dbcb88267b59276ddf
Thiago Feldner <tfeldner@hotmail.com>
2022-09-22 14:04:25 -0300

    inseridos as imagens da tela

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,18 +4,20 @@ import { FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { BookCard, BookCardProps } from '../../components/BookCard';
+import { BookCategorie, BookCategorieProps } from '../../components/BookCategorie';
+import { BooksRead } from '../../components/BooksRead';
 import { Heading } from '../../components/Heading';
-import { BOOKS } from '../../utils/books';
+import { BOOKS, CATEGORIES, MOREREADS } from '../../utils/books';
 
 import { styles } from './styles';
 
 export function Home() {
   const [books, setBooks] = useState<BookCardProps[]>([]);
-  
+   
   useEffect(() => {
     fetch('https://api.nytimes.com/svc/books/v3/lists/names.json\?api-key\=vi0bsV0yOCA9qYnmAaOUJV4dO0BNhUGR')
     .then(response => response.json())
-    .then(data => setBooks(data))    
+    .then(data => console.log(data))
   },[]);
 
   return (
@@ -27,14 +29,40 @@ export function Home() {
         keyExtractor={item => item.title}
         renderItem={({ item })  => (
           <BookCard 
-            data={item}
-            
+            data={item}                        
           />
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.contentList}
       />       
+
+      <FlatList 
+        data={CATEGORIES}
+        keyExtractor = { item => item.title}
+        renderItem ={({ item }) => (
+          <BookCategorie
+            data={item}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+      />
+
+      <FlatList
+        data = {MOREREADS}
+        keyExtractor = { item => item.title}
+        renderItem = {({ item }) => (
+          <BooksRead 
+            data={item}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator = { false }
+        contentContainerStyle = {styles.contentList}
+      />
+      
     </SafeAreaView>
   );
 }
