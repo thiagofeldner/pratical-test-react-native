e35492f30b0042a366a8e7f14b4f96b27ca54f0a
Thiago Feldner <tfeldner@hotmail.com>
2022-09-22 14:06:28 -0300

    inclusão da categoria no arquivo

--- src/utils/books.ts
+++ src/utils/books.ts
@@ -23,5 +23,49 @@ export const BOOKS = [
     author: 'Tulipa Ruiz',
     cover: require('../assets/image-4.png')
   }
+]
+export const MOREREADS =[
+  {
+    id:'5',
+    title: 'O menino e o Foguete',
+    author: 'Marcelo Rubens',
+    cover: require('../assets/image-5.png')
+  },
+  {
+    id:'4',
+    title: 'A Menina das Estrelas',
+    author: 'Tulipa Ruiz',
+    cover: require('../assets/image-4.png')
+  },
+  {
+    id:'6',
+    title: 'A Flor que chegou primeiro',
+    author: 'Mayara Aleluia',
+    cover: require('../assets/image-6.png')
+  },
+  {
+    id: '7',
+    title: 'A árvore generosa',
+    author: 'Shel Silverstein',
+    cover: require('../assets/image-8.png')
+  },
+]
 
+export const CATEGORIES = [
+{
+  title: 'Ficção',
+  cover: require('../assets/categories/Rectangle-1.png')
+},
+{
+  title: 'Tecnologia',
+  cover: require('../assets/categories/Rectangle-2.png')
+},
+{
+  title: 'Biografia',
+  cover: require('../assets/categories/Rectangle-3.png')
+},
+{
+  title: 'Politíca',
+  cover: require('../assets/categories/Rectangle-4.png')
+},
 ]
