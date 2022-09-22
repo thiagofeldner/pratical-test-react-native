d3a1a2bffc88ac0ad4d056c44a22a34fc8ef752a
Thiago Feldner <tfeldner@hotmail.com>
2022-09-22 01:11:26 -0300

    criação do theme para personalizar fonts/cores/size

---
+++ src/theme/index.ts
@@ -0,0 +1,24 @@
+export const THEME = {
+  COLORS: {
+    TEXT: '#333333',
+    PRIMARY: '#673403',
+    SECONDARY: '#00DC00',
+    LOADING: 'black',
+    AUTHOR: '#4F4F4F',
+
+    FOOTER: ['rgba(0,0,0,0)', 'rgba(0,0,0,0.1)'],
+  },
+
+  FONT_FAMILY: {
+    REGULAR: 'Lato_400Regular',
+    BOLD: 'Lato_700Bold',
+    BLACK: 'Lato_900Black'
+  },
+
+  FONT_SIZE: {
+    SM: 12,
+    MD: 14,
+    LG: 18,
+    LLG: 24,
+  }
+}
