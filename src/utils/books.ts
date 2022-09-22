48f2442aaba4c47ada5e7dfaab9700511ae87be8
Thiago Feldner <tfeldner@hotmail.com>
2022-09-22 01:12:01 -0300

    criação de arquivo para trazer info dos livros

---
+++ src/utils/books.ts
@@ -0,0 +1,27 @@
+export const BOOKS = [
+  {
+    id: '1',
+    title: 'Da janela de Minas',
+    author: 'Nicole Rodrigues',
+    cover: require('../assets/image-1.png')
+  },
+  {
+    id: '2',
+    title: 'As bonecas da Vó Maria',
+    author: 'Mel Duarte',
+    cover: require('../assets/image-2.png')
+  },
+  {
+    id: '3',
+    title: 'O apanhador de acalantos',
+    author: 'Beatriz Pereira',
+    cover: require('../assets/image-3.png')
+  },
+  {
+    id:'4',
+    title: 'A Menina das Estrelas',
+    author: 'Tulipa Ruiz',
+    cover: require('../assets/image-4.png')
+  }
+
+]
