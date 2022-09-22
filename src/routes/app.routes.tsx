abf32c2f3c5c27144e7410c4d246b556a5f36c23
Thiago Feldner <tfeldner@hotmail.com>
2022-09-22 02:28:02 -0300

    criado as telas do route

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,22 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Home } from '../screens/Home';
+import { Book } from '../screens/Book';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes(){
+  return (
+    <Navigator>
+      <Screen 
+        name='home'
+        component={Home}
+      />
+
+      <Screen 
+        name='book'
+        component={Book}
+      />
+    </Navigator>
+  )
+}
