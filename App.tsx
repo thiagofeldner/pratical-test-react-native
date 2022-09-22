230d1ee8407feb8d117c9ca0a52738c46c6ddf49
Thiago Feldner <tfeldner@hotmail.com>
2022-09-22 01:17:09 -0300

    alterado p carregar as fontes e a home

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
-import { View, Text, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { useFonts, Lato_400Regular, Lato_700Bold, Lato_900Black } from '@expo-google-fonts/lato';
+
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Lato_400Regular, 
+    Lato_700Bold, 
+    Lato_900Black
+  });
+
   return (
     <View>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
-        translucent
       />
+        
+      {fontsLoaded ? <Home /> : <Loading />}
     </View>
   );
 }
