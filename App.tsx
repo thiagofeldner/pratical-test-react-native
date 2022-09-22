bf0d95f1169857d2a5df1b91efa5454885305c16
Thiago Feldner <tfeldner@hotmail.com>
2022-09-22 14:02:58 -0300

    atualizado a rota no App

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import { View, StatusBar } from 'react-native';
 import { useFonts, Lato_400Regular, Lato_700Bold, Lato_900Black } from '@expo-google-fonts/lato';
 
-import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
+//import { Routes } from './src/routes';
 
 export default function App() {
 
@@ -19,7 +20,9 @@ export default function App() {
         backgroundColor="transparent"
       />
         
-      {fontsLoaded ? <Routes /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
+
+      {/* {fontsLoaded ? <Routes /> : <Loading />} */}
     </View>
   );
 }
